c588dd7b1160397335e5a17396a9ad39964eac88
unknown <emily.reinhart@utexas.edu>
2020-08-12 14:36:07 -0500

    first commit

---
+++ src/components/StartButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyledStartButton } from './styles/StyledStartButton';
+
+const StartButton = ({ callback }) => <StyledStartButton onClick={callback}>start game</StyledStartButton>;
+
+export default StartButton;
